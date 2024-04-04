f477942a51e0b479c6998f9e42de31b44eb7f2cc
1boardapp <79744222+timonharz@users.noreply.github.com>
2024-04-04 21:33:00 +0200

    Update GPTService.py

--- src/search/GPTService.py
+++ src/search/GPTService.py
@@ -1,10 +1,10 @@
 import requests 
 
 
-class GPTService():
+class GPTService:
 
-
-    def getResponse(self, content, role):
+    
+    def getResponse(self):
         api_key = "lo-kud2TUZah6y5zdqTo6cSx3AE9QGn526hxktpn4A9zvT3mwwr"
         url = "https://api.llmos.dev/v1/chat/completions"
 
@@ -12,19 +12,20 @@ class GPTService():
          "Authorization": f"Bearer {api_key}",
           "Content-Type": "application/json"
     }
-        payload = self._getPayload(content, role)
+        payload = self._getPayload("Test", "Assistant")
 
         response = requests.request("POST", url, json=payload, headers=headers)
 
         print(response.text)
 
-    def _getPayload(content, role="Assistant", name="GPTService"):
+    def _getPayload(self, content, role="Assistant", name="GPTService"):
+        print(f"content is: {content} \nrole is: {role} \nname: {name}")
         model = "mistral-7b-instruct"
         payload = {
             "model": model,
         "messages": [
             {
-            "content": content,
+            "content": "Hello World!",
             "role": role,
             "name": name
             }
@@ -35,7 +36,7 @@ class GPTService():
         "top_p": 0,
         "presence_penalty": 0,
         "logit_bias": {},
-        "frequency_penalty": "<string>",
+        "frequency_penalty": 1,
         "false": 0
     }
         print(f"payload: {payload}")
@@ -44,8 +45,7 @@ class GPTService():
 
 
 service = GPTService()
-
-service.getResponse("Hello World", "Assistant")    
+service.getResponse()  
 
    
 
