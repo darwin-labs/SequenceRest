9988de288d103a05e9e2ce202459f797361a02a8
tiharz <79744222+timonharz@users.noreply.github.com>
2024-04-05 23:49:13 +0200

    Update GPTService.py

--- src/search/GPTService.py
+++ src/search/GPTService.py
@@ -1,6 +1,35 @@
 import requests 
+import json
+
+#Data Models for decoding the response from the model
+class ChatCompletion:
+    def __init__(self, data):
+        self.id = data['id']
+        self.model = data['model']
+        self.created = data['created']
+        self.usage = data['usage']
+        self.object = data['object']
+        self.choices = [Choice(choice_data) for choice_data in data['choices']]
+
+class Choice:
+    def __init__(self, choice_data):
+        self.index = choice_data['index']
+        self.finish_reason = choice_data['finish_reason']
+        self.message = Message(choice_data['message'])
+        self.delta = Delta(choice_data['delta'])
+
+class Message:
+    def __init__(self, message_data):
+        self.role = message_data['role']
+        self.content = message_data['content']
+
+class Delta:
+    def __init__(self, delta_data):
+        self.role = delta_data['role']
+        self.content = delta_data['content']
 
 
+#GPTService for handling any type requests
 class GPTService:
 
     
@@ -12,11 +41,16 @@ class GPTService:
          "Authorization": f"Bearer {api_key}",
           "Content-Type": "application/json"
     }
-        payload = self._getPayload("Test", "Assistant")
+        payload = self._getPayload("Hello World!", "user")
 
         response = requests.request("POST", url, json=payload, headers=headers)
 
-        print(response.text)
+        json_string = response.text
+
+        json_content = json.loads(json_string)
+        
+        return json_content
+        
 
     def _getPayload(self, content, role="Assistant", name="GPTService"):
         print(f"content is: {content} \nrole is: {role} \nname: {name}")
@@ -25,19 +59,17 @@ class GPTService:
             "model": model,
         "messages": [
             {
-            "content": "Hello World!",
+            "content": content,
             "role": role,
-            "name": name
+            "name": "GPTService - Sequence"
             }
         ],
-        "max_tokens": 5000,
+        "max_tokens": 500,
         "stream": True,
-        "temperture": 1,
+        "temperture": 123,
         "top_p": 0,
         "presence_penalty": 0,
-        "logit_bias": {},
-        "frequency_penalty": 1,
-        "false": 0
+        "logit_bias": {}
     }
         print(f"payload: {payload}")
         return payload
@@ -45,12 +77,5 @@ class GPTService:
 
 
 service = GPTService()
-service.getResponse()  
-
-   
-
-
-
-
-
-
+json_content = service.getResponse()
+chat_completion = ChatCompletion(json)
