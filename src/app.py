d4bc3f0bef9b6f524a71df5676936a988fe22851
1boardapp <79744222+timonharz@users.noreply.github.com>
2024-05-05 21:39:42 +0200

    Chnages

--- src/app.py
+++ src/app.py
@@ -15,6 +15,7 @@ def query_request():
     query = request.args.get('query')
     num_of_sources = request.args.get('num')
     print(query)
+    return f"Query: {query}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=False)
