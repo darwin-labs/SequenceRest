916e096feeeae9458833a06510a78669509a8a1c
timonharz <timon.harz@icloud.com>
2024-06-16 21:59:11 +0200

    chnages

--- src/Tests/GoogleSearchSpeedEvaluationTest.py
+++ src/Tests/GoogleSearchSpeedEvaluationTest.py
@@ -6,7 +6,7 @@ if __name__ == '_main__':
 
     result = search(query, num_results=5)
     
-    # Print the results
-print(f"Top {len(list(results))} results for '{query}':")
-for i, result in enumerate(results, start=1):
-    print(f"{i}. {result}")
+     Print the results
+    print(f"Top {len(list(results))} results for '{query}':")
+    for i, result in enumerate(results, start=1):
+        print(f"{i}. {result}")
